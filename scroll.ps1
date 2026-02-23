0d09bde80be6576ebea14921d2ed19b4f02d0587
Balazs <balzsovd@gmail.com>
2026-02-23 18:23:37 +0100

    Update scroll.ps1

--- scroll.ps1
+++ scroll.ps1
@@ -1,8 +1,38 @@
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+
+public class IdleTime {
+    [StructLayout(LayoutKind.Sequential)]
+    struct LASTINPUTINFO {
+        public uint cbSize;
+        public uint dwTime;
+    }
+
+    [DllImport("user32.dll")]
+    static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+    public static uint GetIdleTime() {
+        LASTINPUTINFO lii = new LASTINPUTINFO();
+        lii.cbSize = (uint)Marshal.SizeOf(lii);
+        GetLastInputInfo(ref lii);
+        return ((uint)Environment.TickCount - lii.dwTime);
+    }
+}
+"@
+
 Add-Type -AssemblyName System.Windows.Forms
 
+$idleThreshold = 180000  # 3 minutes in milliseconds
+
 while ($true) {
-    Start-Sleep -Seconds 240
-    [System.Windows.Forms.SendKeys]::SendWait("{SCROLLLOCK}")
-    Start-Sleep -Milliseconds 200
-    [System.Windows.Forms.SendKeys]::SendWait("{SCROLLLOCK}")
+    Start-Sleep -Seconds 5
+
+    $idleTime = [IdleTime]::GetIdleTime()
+
+    if ($idleTime -ge $idleThreshold) {
+        [System.Windows.Forms.SendKeys]::SendWait("{SCROLLLOCK}")
+        Start-Sleep -Milliseconds 200
+        [System.Windows.Forms.SendKeys]::SendWait("{SCROLLLOCK}")
+    }
 }
